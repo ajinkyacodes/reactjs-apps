bd59d159bde30217d304aed887f56f2f71f2a469
AJINKYA <ajinkyacodes@gmail.com>
2023-12-18 13:32:12 +0530

    used react-icons for comment and like. Added dog pic for like photo app

--- src/projects/03-Like-My-Photo/LikePhotoApp.jsx
+++ src/projects/03-Like-My-Photo/LikePhotoApp.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React from "react";
 import Title from "../../components/Title";
+import { AiFillSmile, AiFillHeart, AiOutlineHeart, AiOutlineComment } from "react-icons/ai";
+import Photo from "./img/dog.jpg";
+
 export default function LikePhotoApp() {
+  let like=true;
   return (
     <div className="conatianer text-center">
       <Title text={"Like Photo App"} />
-      <Title classes= {"subtitle"} text={"Likes"} />
-      <div className="card card-dark m-auto" style={{width:300, cursor:"pointor"}}>
+      <Title classes={"subtitle"} text={"Likes"} />
+      <div
+        className="card card-dark m-auto"
+        style={{ width: 300, cursor: "pointor" }}
+      >
         <div className="card-header fs-xl">
+          <AiFillSmile className="mr-2" />
           <small>DogyDog</small>
         </div>
-        <img src="" alt="img" style={{height:"fit-content"}} />
-        <div className="card-footer fs-xl d-flex" style={{justifyContent: "space-between"}}>
-
+        <img src={Photo} alt="img" style={{ height: "fit-content" }} />
+        <div
+          className="card-footer fs-xl d-flex"
+          style={{ justifyContent: "space-between" }}
+        >
+          <AiOutlineComment />{" "}
+          {like ? <AiFillHeart className="text-danger" /> : <AiOutlineHeart />}
         </div>
       </div>
     </div>
-  )
+  );
 }
