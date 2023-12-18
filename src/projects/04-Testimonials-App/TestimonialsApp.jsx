5ee5403acbf9b7760548bff4d798cb0cf483e50e
AJINKYA <ajinkyacodes@gmail.com>
2023-12-18 17:41:42 +0530

    Fetched JSON Placeholders API dummy data for displaying on project

--- src/projects/04-Testimonials-App/TestimonialsApp.jsx
+++ src/projects/04-Testimonials-App/TestimonialsApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Title from "../../components/Title";
 import Button from "../../components/Button";
 import {BsFillFileEarmarkPostFill} from "react-icons/bs";
@@ -6,15 +6,40 @@ import {FaUserAlt} from "react-icons/fa";
 import {BiCommentDetail} from "react-icons/bi";
 
 export default function TestimonialsApp() {
-  function handleClick(){
-    console.log("My Click");
-  }
+  
+  const [testimonials, setTestimonials] = useState("");
+  const [items, setItems] = useState();
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${testimonials}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [testimonials]);
+
   return (
     <div className="container">
       <Title text={"Testimonials App"} />
-      <Button text={"Posts"} classes={"btn-info"} icon={<BsFillFileEarmarkPostFill className="mr-1"/>} onClick={handleClick} />
-      <Button text={"Users"} classes={"btn-info"} icon={<FaUserAlt className="mr-1"/>} onClick={handleClick} />
-      <Button text={"Comments"} classes={"btn-info"} icon={<BiCommentDetail className="mr-1"/>} onClick={handleClick} />
+      <Button text={"Posts"} classes={"btn-info"} icon={<BsFillFileEarmarkPostFill className="mr-1"/>} onClick={()=>setTestimonials("Posts")} />
+      <Button text={"Users"} classes={"btn-info"} icon={<FaUserAlt className="mr-1"/>} onClick={()=>setTestimonials("Users")} />
+      <Button text={"Comments"} classes={"btn-info"} icon={<BiCommentDetail className="mr-1"/>} onClick={()=>setTestimonials("Comments")} />
+      <Title text={!testimonials ? "Select from above!" : testimonials} classes={"subtitle text-primary"}/>
+     
+      {!items
+        ? null
+        : items.map((item) => {
+            return (
+            <div className="card card-primary mb-2" key={item.id}>              
+                {item.name && <h2 className="card-header">{item.name}</h2>}
+                <div className="card-body">
+                  <h4>{item.title}</h4>
+                  <p>{item.body}</p>
+                </div>
+                {item.email && (
+                  <small className="card-footer">{item.email}</small>
+                )}
+            </div>
+          );
+      })}
     </div>
-  )
+  );
 }
