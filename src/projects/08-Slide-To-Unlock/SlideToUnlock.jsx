0fa7c0a38c66b2e6e024fce7be95a89ee8d6ad91
AJINKYA <ajinkyacodes@gmail.com>
2023-12-22 01:12:01 +0530

    created lock screen state

--- src/projects/08-Slide-To-Unlock/SlideToUnlock.jsx
+++ src/projects/08-Slide-To-Unlock/SlideToUnlock.jsx
@@ -1,23 +1,34 @@
 import React, { useState } from "react";
 import LockSlider from "./LockSlider";
 import { AiFillUnlock } from "react-icons/ai";
+import LockScreenImg from "./img/moon.jpg";
+import HomeScreenImg from "./img/home.jpg";
+
 export default function SlideToUnlock() {
-  const [showLockSlider, setShowLockSlider] = useState(0);
+  const [uiProps, setUiProps] = useState({
+    uiText: "Unlock Screen",
+    uiColor: "#eee",
+    uiBg: `Url(${LockScreenImg}) center/cover no-repeat`
+  });
+  const [showLockSlider, setShowLockSlider] = useState(true);
 
   return (
     <div
-      className="container text-center d-flex flex-column border-20"
+      className="container text-center d-flex flex-column border-20 shadow-md"
       style={{
         height: "70vh",
         marginTop: "15vh",
         width: 340,
         border: "4px solid #000",
+        background: uiProps.uiBg,
       }}
     >
-      <h1 className="title">title</h1>
-      { showLockSlider ? ( <LockSlider width={"250px"} /> ) : ( <AiFillUnlock className="unlockIcon" /> ) }
-      <LockSlider width={"250px"} />
-      <AiFillUnlock className="unlockIcon" />
+      <h1 className="title" style={{ color: uiProps.uiColor }} >{uiProps.uiText}</h1>
+      {showLockSlider ? (
+        <LockSlider width={"250px"} />
+      ) : (
+        <AiFillUnlock className="unlockIcon" />
+      )}
     </div>
   );
 }
