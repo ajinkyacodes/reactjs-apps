093c725f98be202849637927945300a8aa018738
AJINKYA <ajinkyacodes@gmail.com>
2023-12-21 11:18:56 +0530

    Temperature Controller functionality added using useState

--- src/projects/06-Temperature-Controller/TermeratureController.jsx
+++ src/projects/06-Temperature-Controller/TermeratureController.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import Button from '../../components/Button'
+import React, { useState } from "react";
+import Button from "../../components/Button";
 
 export default function TermeratureController() {
+  const [temperature, setTemperature] = useState(0);
+
+  const increaseTemperature = () => {
+    setTemperature(temperature+1);
+  }
+
+  const decreaseTemperature = () => {
+    setTemperature(temperature-1);
+  }
+
   return (
-    <div className='container mt-3 text-center'>
-        <div className="card bg-light m-auto" style={{ width: 200 }}>
-            <h1 className='text-light card border-50' style={{ height:150, width:150, border:"2px solid #666" }}>Temperature</h1>
-            <div className="d-flex my-2">
-                <Button text='-' classes={"btn-lg"} onClick={()=>console.log("-")} />
-                <Button text='+' classes={"btn-lg"} onClick={()=>console.log("+")} />
-            </div>
+    <div className="container mt-3 text-center">
+      <div className="card bg-light m-auto" style={{ width: 200 }}>
+        <h1
+          className={`text-light card border-50 ${temperature > 0 ? "bg-danger" : "bg-info"}`}
+          style={{ height: 150, width: 150, border: "2px solid #666" }}
+        >
+          {temperature}° C
+        </h1>
+        <div className="d-flex my-2">
+          <Button
+            text="-"
+            classes={"btn-lg"}
+            onClick={decreaseTemperature}
+          />
+          <Button
+            text="+"
+            classes={"btn-lg"}
+            onClick={increaseTemperature}
+          />
         </div>
+      </div>
     </div>
-  )
+  );
 }
