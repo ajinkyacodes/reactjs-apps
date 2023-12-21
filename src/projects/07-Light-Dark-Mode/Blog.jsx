79ad747536e7a6c798091a191bb7e70875b17a0f
AJINKYA <ajinkyacodes@gmail.com>
2023-12-21 12:23:47 +0530

    Used createContext for dark and light theme

--- src/projects/07-Light-Dark-Mode/Blog.jsx
+++ src/projects/07-Light-Dark-Mode/Blog.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import Title from "../../components/Title";
 import Button from "../../components/Button";
-export default function Blog({ theme }) {
+import { ThemeContext } from "./context/theme-context";
+export default function Blog() {
+  const {theme, changeTheme} = useContext(ThemeContext);
   return (
     <div className="container p-1">
       <Title text={`My Blog with ${theme} Theme`} />
       <span style={{position: "absolute", top:10, right: 10}}>
-        <Button text={theme} />
+        <Button text={theme === "dark" ? "Light" : "Dark"} classes={ `${theme==="dark" && "btn-light"} btn-sm`} onClick={changeTheme} />
       </span>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet doloremque vel doloribus deserunt provident at aliquam repudiandae ullam cum eos et facere atque nisi iusto totam eius, unde impedit vitae!</p>
     </div>
