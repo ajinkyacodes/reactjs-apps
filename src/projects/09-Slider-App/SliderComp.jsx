23f90bcfe55bafbbc4e9f0793f1b70cffc4c9f00
AJINKYA <ajinkyacodes@gmail.com>
2023-12-23 01:02:35 +0530

    Slide to grow functionality created using useState hook

--- src/projects/09-Slider-App/SliderComp.jsx
+++ src/projects/09-Slider-App/SliderComp.jsx
@@ -1,21 +1,44 @@
 import React from "react";
 
-export default function SliderComp() {
+export default function SliderComp({
+  setValue,
+  handleInput,
+  bgColor,
+  textColor,
+}) {
+    const slideStyle = {
+        appearance: "none",
+        width: "100%",
+        height: 18,
+        background: "lightgray",
+        cursor: "pointer",
+        opacity: 0.7,
+        borderRadius: "25px",
+    };
   return (
     <div className="d-flex flex-column" style={{ gap: 100 }}>
-      <input type="range" min="0" max={100} value={0} />
-      <div style={{
-        color: "black",
-        background:"lightgray",
-        height: `30px`,
-        width: `30px`,
-        fontWeight: 600,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: "50%",
-      }}>
-        <span>circle</span>
+      <input
+        type="range"
+        min="0"
+        max={100}
+        value={setValue}
+        onInput={handleInput}
+        style={slideStyle}
+      />
+      <div
+        style={{
+          color: !textColor ? "black" : textColor,
+          background: !bgColor ? "lightgray" : bgColor,
+          height: `${setValue * 3}px`,
+          width: `${setValue * 3}px`,
+          fontWeight: 600,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: "50%",
+        }}
+      >
+        <span>{setValue}</span>
       </div>
     </div>
   );
