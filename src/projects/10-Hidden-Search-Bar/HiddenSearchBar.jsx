a7097730abee3bdf15f29a86104ea6d3582ec2dc
AJINKYA <ajinkyacodes@gmail.com>
2023-12-24 22:37:22 +0530

    created static ui for searchbar with searchbar icon

--- src/projects/10-Hidden-Search-Bar/HiddenSearchBar.jsx
+++ src/projects/10-Hidden-Search-Bar/HiddenSearchBar.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import { BsSearch } from "react-icons/bs";
 
 export default function HiddenSearchBar() {
+  let body = document.body.style.background = "purple";
+  let inputStyle = {
+    margin: "10vh 25vw",
+    wifth: "20vh",
+    height: "30px",
+    padding: "1rem .3rem",
+    border: "none",
+    outline: "none",
+    background: "transparent",
+    borderBottom: `1px solid #333`,
+    fontSize: "1.3rem",
+    color: "#eee",
+    boxShadow: "0px 55px 60px -15px rgba(0,0,0,.75)",
+    opacity: 0.9,
+    transition: "all .3s ease",
+  };
+
+  let BsSearchStyle = {
+    // color: "#fff",
+    fontSize: 50,
+    cursor: "pointer",
+    position: "absolute",
+    top: 20,
+    right: 20,
+  };
+
   return (
-    <div>Hidden Search Bar</div>
-  )
+    <div className="container">
+      <input type="text" placeholder="Search" style={inputStyle} />
+      <BsSearch style={BsSearchStyle} />
+    </div>
+  );
 }
