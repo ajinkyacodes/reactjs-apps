7f09e0c70ddc4b6c05352c9969c5f106b2d39ca5
AJINKYA <ajinkyacodes@gmail.com>
2024-04-27 12:52:20 +0530

    input fields individual validation done

--- src/projects/15-Reg-Form-Validation/RegForm.jsx
+++ src/projects/15-Reg-Form-Validation/RegForm.jsx
@@ -1,69 +1,101 @@
-import React, {useRef, useEffect, useState} from 'react';
-import Title from "../../components/Title";
-import Button from "../../components/Button";
-import Alert from "../../components/Alert";
+import React, { useRef, useEffect, useState } from 'react';
+import Title from '../../components/Title';
+import Button from '../../components/Button';
+import Alert from '../../components/Alert';
 
 export default function RegForm() {
-
 	let inputName = useRef(null);
 	let inputEmail = useRef(null);
 	let inputPassword = useRef(null);
 
-	useEffect(()=>{
+	useEffect(() => {
 		inputName.current.focus();
-	},[]);
+	}, []);
 
 	const [values, setValues] = useState({
-		user_name: "",
-		email: "",
-		password: ""
+		user_name: '',
+		email: '',
+		password: '',
 	});
 
 	const handleNameChange = (e) => {
-		setValues({...values, user_name: e.target.value });
-	}
+		setValues({ ...values, user_name: e.target.value });
+	};
 
 	const handleEmailChange = (e) => {
-		setValues({...values, email: e.target.value });
-	}
+		setValues({ ...values, email: e.target.value });
+	};
 
 	const handlePasswordChange = (e) => {
 		let value = e.target.value;
-		setValues({...values, password: value });
-	}
+		setValues({ ...values, password: value });
+	};
+
+	const [submit, setSubmit] = useState(false);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		if(!values.user_name){
+		if (!values.user_name) {
 			inputName.current.focus();
-		} else if(!values.email){
+		} else if (!values.email) {
 			inputEmail.current.focus();
-		} else if(!values.password){
+		} else if (!values.password) {
 			inputPassword.current.focus();
 		}
-	}
+		setSubmit(true);
+	};
 
-	return <div className='m-auto'>
-		
-		{/* Form Design */}
-		<div className="container m-auto mt-4 p-2 shadow-sm border-5">
-			<Title text="Registration" />
-			<form onSubmit={handleSubmit}>
-				<div className="form-group">
-					<input type="text" placeholder="Enter your name" ref={inputName} value={values.user_name} onChange={handleNameChange} />
-					<label className='text-danger fs-sm'>Please enter your name</label>
-				</div>
-				<div className="form-group">
-					<input type="email" placeholder="Enter your email" ref={inputEmail} value={values.email} onChange={handleEmailChange} />
-					<label className='text-danger fs-sm'>Please enter your email</label>
-				</div>
-				<div className="form-group">
-					<input type="password" placeholder="Enter your password" ref={inputPassword} value={values.password} onChange={handlePasswordChange} />
-					<label className='text-danger fs-sm'>Please enter your password</label>
-				</div>
-				<Button classes={"btn-primary btn-lg btn-block"} text="Register" />
-			</form>
+	return (
+		<div className='m-auto'>
+			{/* Form Design */}
+			<div className='container m-auto mt-4 p-2 shadow-sm border-5'>
+				<Title text='Registration' />
+				<form onSubmit={handleSubmit}>
+					<div className='form-group'>
+						<input
+							type='text'
+							placeholder='Enter your name'
+							ref={inputName}
+							value={values.user_name}
+							onChange={handleNameChange}
+						/>
+						{submit && !values.user_name ? (
+							<label className='text-danger fs-sm'>
+								Please enter your name
+							</label>
+						) : null}
+					</div>
+					<div className='form-group'>
+						<input
+							type='email'
+							placeholder='Enter your email'
+							ref={inputEmail}
+							value={values.email}
+							onChange={handleEmailChange}
+						/>
+						{submit && !values.email ? (
+							<label className='text-danger fs-sm'>
+								Please enter your email
+							</label>
+						) : null}
+					</div>
+					<div className='form-group'>
+						<input
+							type='password'
+							placeholder='Enter your password'
+							ref={inputPassword}
+							value={values.password}
+							onChange={handlePasswordChange}
+						/>
+						{submit && !values.password ? (
+							<label className='text-danger fs-sm'>
+								Please enter your password
+							</label>
+						) : null}
+					</div>
+					<Button classes={'btn-primary btn-lg btn-block'} text='Register' />
+				</form>
+			</div>
 		</div>
-
-		</div>;
+	);
 }
